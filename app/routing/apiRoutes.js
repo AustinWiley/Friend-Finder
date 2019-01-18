775580368212c8cc22826c96713a361c5acda9a0
A Wiley <42783142+AustinWiley@users.noreply.github.com>
2019-01-18 14:40:58 -0700

    initial server js code

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,6 @@
 let friendsData = require('../data/friends');
 
-module.exports = function () {
+module.exports = function (app) {
     //get request.  collects data from `friends data`, turn it into json and send it back to teh front end(the browser)
     app.get('/api/friends' function(req,res) {
         res.json(frindsData);
