35d414fcd97083ca0ee56d8b3018fa339529bd40
A Wiley <42783142+AustinWiley@users.noreply.github.com>
2019-01-21 18:23:07 -0700

    commit

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded(({ extended: true })));
+app.use(express.urlencoded(({
+    extended: true
+})));
 app.use(express.json());
 
 //point server to routes files
@@ -14,6 +16,15 @@ require('./app/routing/apiRoutes')(app);
 require('./app/routing/htmlRoutes')(app);
 
 //LISTENER. effectively 'starts' server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log('App listening on PORT: ' + PORT);
 });
+
+// use this in post to compare numbers
+function foo(num1, num2) {
+    if (num1 > num2) return num1 - num2
+    return num2 - num1
+}
+
+console.log(foo(1, 8));
+console.log(foo(8, 1));
